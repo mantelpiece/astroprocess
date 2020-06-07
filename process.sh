9b5e328adce35b25c6d30e140845382871775f51
Brendan Studds <brendan.studds@gmail.com>
2020-06-07 17:04:49 +1000

    Many updates to process script
    * Print out config before proceeding
    * Look for master calibration frames and use those
    * Split light processing so intermediate files are deleted sooner

--- process.sh
+++ process.sh
@@ -10,140 +10,33 @@ die () { errr "${1:-""}" >&2; exit "${2:-1}"; }
 usage () { die "\e[0m$0 -p IMAGING_PATH [-b MASTER_BIAS] [-f MASTER_FLAT] [-d MASTER_DARK]"; }
 
 hash siril 2>/dev/null || { die "Mising dep: siril"; }
+
+
 siril_w () { siril-cli -s <(echo "$*") >&2; }
 
 
 
-precheck () {
-  maybeMasterFlat="$currentDir/$imagingPath/Flats/master-flat.fit"
-  if [[ -f "$maybeMasterFlat" ]]; then
-    masterFlat="$maybeMasterFlat"
-    info "$masterFlat exists... use as master flat"
-  else
-    info "no master flat found"
-  fi
-
-  maybeMasterDark="$currentDir/$imagingPath/Darks/master-dark.fit"
-  if [[ -f "$maybeMasterDark" ]]; then
-    masterDark="$maybeMasterDark"
-    info "$masterDark exists... use as master dark"
-  else
-    info "no master dark found"
-  fi
-}
-
-
-main () {
-  if [[ -z "$masterBias" ]] && [[ -n "$masterFlat" ]]; then
-    info "Generating bias $masterBias"
-    masterBias="$(generateMasterBias)"
-  fi
-
-  if [[ -z "$masterFlat" ]]; then
-    info "Generating master flat $masterFlat..."
-    masterFlat="$(generateMasterFlat)"
-    good "Created $masterFlat"
-  fi
-
-  if [[ -z "$masterDark" ]]; then
-    info "Generating master dark $masterDark"
-    masterDark="$(generateMasterDark)"
-  fi
-
-  info "Processing lights..."
-  if ! output=$(processLights); then
-    die "********* GENERATION FAILED"
-  fi
-  good "****************** SUCCESS *******************"
-  echo "output: $output"
-}
-
-
-
-generateMasterBias () {
-  biasesScript="convertraw bias_
-stack bias_ rej 3 3 -nonorm -out=master-bias.fit"
-
-  (
-    cd "$biasDir";
-    if ! siril_w "$biasesScript"; then
-      rm -f bias_*
-      die "Siril error"
-    fi
-    rm -f bias_*
-    echo "$(pwd)/master-bias.fit"
-  )
-  masterBias="$imagingPath/Bias/master-bias.fit"
-}
-
-generateMasterFlat () {
-  flatsScript="convertraw flat_
-preprocess flat_ -bias=$currentDir/$masterBias
-stack pp_flat_ rej 3 3 -norm=mul -out=master-flat.fit"
-
-  (
-    cd "$imagingPath/Flats"
-    if ! siril_w "$flatsScript"; then
-      rm -f flat_* pp_flat_*
-      die "Siril error processing flats"
-    fi
-    rm -f flat_* pp_flat_*
-  )
-  masterFlat="$currentDir/$imagingPath/Flat/master-flat.fit"
-}
-
-
-generateMasterDark () {
-  darksScript="convertraw dark_
-stack dark_ rej 3 3 -nonorm -out=master-dark.fit"
-
-  (
-    info "Using master bias - $masterBias" >&2
-    cd "$imagingPath/Darks";
-    if ! siril_w "$darksScript"; then
-      rm -f dark_*
-      die "Siril error while generating master dark"
-    fi
-    rm -f dark_*
-  )
-  masterDark="$currentDir/$imagingPath/Dark/master-dark.fit"
-}
-
-
-processLights () {
-  lightsScript='convertraw light_
-preprocess light_ -dark='"$masterDark"' -flat='"$masterFlat"' -cfa -equalize_cfa -debayer
-register pp_light_
-stack r_pp_light_ rej 3 3 -norm=addscale -out=../Stacks/stack.fit'
-info "***** Light processing script ******" >&2
-echo "$lightsScript" >&2
-info "*****                         ******" >&2
-
-  (
-    info "Using master flat - $masterFlat" >&2
-    info "Using master dark - $masterDark" >&2
-    cd "$imagingPath/Lights"
-    if ! siril_w "$lightsScript"; then
-      rm -f light_* pp_light_* r_pp_light_*
-      die "Siril failed during lights preprocessing"
-    fi
-    rm -f light_* pp_light_* r_pp_light_*
-
-    echo "$(pwd)/../Stacks/final-stack.fit"
-  )
-}
-
-
+# Initialise
 imagingPath=
-masterBias=
+lightsPath="Lights"
+biasPath="Biases"
+flatsPath="Flats"
+darksPath="Darks"
+session=
+masterBias="/mnt/c/Users/Brendan/Documents/astrophotography/Imaging/calibration_library/bias/iso800/master-bias_iso800_134frames.fit"
 masterFlat=
 masterDark=
-while getopts "p:b:f:d:" i; do
+while getopts "p:L:F:D:B:b:f:d:S" i; do
   case "$i" in
     p) imagingPath="$OPTARG" ;;
+    L) lightsPath="$OPTARG" ;;
+    B) biasesPath="$OPTARG" ;;
+    F) flatsPath="$OPTARG" ;;
+    D) darksPath="$OPTARG" ;;
     b) masterBias="$OPTARG" ;;
     f) masterFlat="$OPTARG" ;;
     d) masterDark="$OPTARG" ;;
+    S) session="${OPTARG:-session}" ;;
     -) break ;;
     ?) usage ;;
     *) usage ;;
@@ -152,6 +45,159 @@ done
 [[ -n "$imagingPath" ]] || { usage; }
 [[ -d $imagingPath ]] || { die "$imagingPath not found"; }
 currentDir=$(pwd)
+stackName="stack_$(date +"%Y-%m-%dT%H:%m.fit")"
+[[ $stackName =~ "stack_" ]] || { die "Failed to generate output stack name"; }
 
-precheck
-main
+info "**** Processing configuration ****"
+
+if [[ -z "$masterBias" ]]; then
+  die "No master bias"
+else
+  echo "master bias:
+    using $masterBias"
+fi
+
+maybeMasterFlat="$currentDir/$imagingPath/$flatsPath/master-flat.fit"
+if [[ -f "$maybeMasterFlat" ]]; then
+  masterFlat="$maybeMasterFlat"
+  echo "master flat:
+    found $masterFlat... use as master flat"
+else
+  echo "master flat - no master flat found:
+    processing $imagingPath/$flatsPath"
+fi
+
+maybeMasterDark="$currentDir/$imagingPath/Darks/master-dark.fit"
+if [[ -f "$maybeMasterDark" ]]; then
+  masterDark="$maybeMasterDark"
+  echo "master dark:
+    found $masterDark... use as master dark"
+else
+  echo "master dark - no master dark found:
+    processing $imagingPath/$darksPath"
+fi
+
+echo "processing lights:
+    $imagingPath/$lightsPath"
+
+[[ -n "$session" ]] && echo -e "\nrunning in session mode, only pre-processing will be applied to lights and they will not be registered or stacked"
+
+
+echo -e "\nOutput stack will be $imagingPath/Stacks/$stackName"
+
+echo -ne "\n\nContinue processing? .... "
+read -r input
+
+
+
+if [[ -z "$masterBias" ]] && [[ -n "$masterFlat" ]]; then
+  info "\n**** Generating master bias ****"
+  masterBias="$(generateMasterBias)"
+  biasesScript="convertraw bias_
+stack bias_ rej 3 3 -nonorm -out=master-bias.fit"
+
+  echo "Master bias generation script:
+$biasesScript"
+
+  (
+    cd "$imagingPath/$biasesPath";
+    if ! siril_w "$biasesScript"; then
+      rm -f bias_*
+      die "Siril error generating master bias"
+    fi
+    rm -f bias_*
+    echo "$(pwd)/master-bias.fit"
+  )
+
+  masterBias="$biasesPath/master-bias.fit"
+  good "Created master bias $masterBias"
+fi
+
+
+if [[ -z "$masterFlat" ]]; then
+  info "\n**** Generating master flat ****"
+
+  flatsScript="convertraw flat_
+preprocess flat_ -bias=$masterBias
+stack pp_flat_ rej 3 3 -norm=mul -out=master-flat.fit"
+  echo "Master flat generation script
+$flatsScript"
+
+  (
+    cd "$imagingPath/$flatsPath"
+    if ! siril_w "$flatsScript"; then
+      rm -f flat_* pp_flat_*
+      die "Siril error processing flats"
+    fi
+    rm -f flat_* pp_flat_*
+  )
+  masterFlat="../$flatsPath/master-flat.fit"
+  good "Created master flat $masterFlat"
+fi
+
+
+
+if [[ -z "$masterDark" ]]; then
+  info "\n**** Generating master dark ****"
+
+  darksScript="convertraw dark_
+stack dark_ rej 3 3 -nonorm -out=master-dark.fit"
+  echo "Master dark script
+$darksScript"
+
+  (
+    cd "$imagingPath/$darksPath";
+    if ! siril_w "$darksScript"; then
+      rm -f dark_*
+      die "Siril error while generating master dark"
+    fi
+    rm -f dark_*
+  )
+  masterDark="../$darksPath/master-dark.fit"
+fi
+
+
+
+info "\n**** Processing lights ****"
+  preprocess="convertraw light_
+preprocess light_ -dark=$masterDark -flat=$masterFlat -cfa -equalize_cfa -debayer"
+  register="register pp_light_"
+  stack="stack r_pp_light_ rej 3 3 -norm=addscale -out=../Stacks/$stackName"
+
+
+mkdir -p $imagingPath/Stacks
+
+info "\n***** Light processing script ******"
+echo "$preprocess"
+[[ -z "$session" ]] && echo -e "$register\n$stack"
+
+
+info "\n**** Begin light processing ****"
+
+(
+  cd "$imagingPath/$lightsPath"
+    info "\n**** Converting and preprocessing lights ****"
+  if ! siril_w "$preprocess"; then
+    rm -f light_* pp_light_*
+    die "Siril failed during lights preprocessing"
+  fi
+  rm -f light_*
+
+  if [[ -z "$session" ]]; then
+    info "\n**** Registering and stacking lights ****"
+    if ! siril_w "$register"; then
+      rm -f r_pp_light_*
+      die "Siril failed during register of lights"
+    fi
+
+    rm -f pp_light_*
+    if ! siril_w "$stack"; then
+      die "Siril failed during lights stacking"
+    fi
+
+    rm -f light_* pp_light_* r_pp_light_*
+  fi
+
+)
+good "**** Success ****"
+good "Final stack $imagingPath/Stacks/final-stack.fit"
