116aa3c741ca8cf9fa60be3e93904eac2e2ad2ac
Brendan Studds <brendan.studds@gmail.com>
2021-05-24 12:02:03 +1000

    [WIP] new script to generate masters

--- 01-generate-masters.sh
+++ 01-generate-masters.sh
@@ -4,8 +4,19 @@
 die () { echo "$1" >&2; exit 1; }
 
 
-:${noFlats=""}
-:${masterFlat=""}
+if hash siril-cli 2>/dev/null; then
+  sirilBin="siril-cli"
+elif hash siril 2>/dev/null; then
+  sirilBin="siril"
+else
+  die "Missing dep: siril"
+fi
+
+noBiases=${noBiases=""}
+noFlats=${noFlats=""}
+masterBias=${masterBias=""}
+masterFlat=${masterFlat=""}
+
 
 # Convert NEF to FITS (no demosaicing)
 
@@ -24,6 +35,20 @@ die () { echo "$1" >&2; exit 1; }
 #   * use cosmetic correction
 #   * cfa, debayer, equalize_cfa
 #   * stacking: average stacking with rejection, winsorized sigma clipping sigma low/hi 4/3
+
+
+:'                                                              LIGHTS
+DARKS                /-> sub()* -> sum()                           | -> sub()
+BIASES      -> sum()                                               | -> sub()
+FLATS               \-> sub()           /-> sub()    -> sum()      | -> div()
+DARKFLATS            \-> sub()  -> sum()                           |
+                                                                   |-> register() -> stack()
+'
+
+
+if [[ -z $noBiases && -z $masterBias ]]; then
+    true
+fi
 
 if [[ -z $noFlats && -z $masterFlat ]]; then
     if [[ -z $masterBias ]]; then
