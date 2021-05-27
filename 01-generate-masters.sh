e522de9ef50d6a881fc89520daccb3175f25353e
Brendan Studds <brendan.studds@gmail.com>
2021-05-27 23:43:21 +1000

    Fixing 01-generate-masters.sh

--- 01-generate-masters.sh
+++ 01-generate-masters.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+set -u
+
+good () { echo -e "\e[32m$*\e[0m"; }
+info () { echo -e "\e[34m$*\e[0m"; }
+errr () { echo -e "\e[31m$*\e[0m"; }
 
 die () { echo "$1" >&2; exit 1; }
 
 
+hash jq 2>/dev/null || die "Missing dependency: jq";
 if hash siril-cli 2>/dev/null; then
   sirilBin="siril-cli"
 elif hash siril 2>/dev/null; then
@@ -12,10 +18,8 @@ else
   die "Missing dep: siril"
 fi
 
-noBiases=${noBiases=""}
-noFlats=${noFlats=""}
-masterBias=${masterBias=""}
-masterFlat=${masterFlat=""}
+
+dir="$(dirname "$0")"
 
 
 # Convert NEF to FITS (no demosaicing)
@@ -26,7 +30,7 @@ masterFlat=${masterFlat=""}
 # master dark:
 #   * stacking with median algorithm (or Winsorized with rejection <0.5%)
 #   * no normalisation
-# master dark:
+# master flat:
 #   * subtract master bias
 #   * stacking with median algorithm (or Winsorized with rejection <0.5%)
 #   * multiplicated normalisation
@@ -37,23 +41,88 @@ masterFlat=${masterFlat=""}
 #   * stacking: average stacking with rejection, winsorized sigma clipping sigma low/hi 4/3
 
 
-:'                                                              LIGHTS
-DARKS                /-> sub()* -> sum()                           | -> sub()
-BIASES      -> sum()                                               | -> sub()
-FLATS               \-> sub()           /-> sub()    -> sum()      | -> div()
-DARKFLATS            \-> sub()  -> sum()                           |
-                                                                   |-> register() -> stack()
-'
+#                                                                 LIGHTS
+# DARKS                /-> sub()* -> sum()                           | -> sub()
+# BIASES      -> sum()                                               | -> sub()
+# FLATS               \-> sub()           /-> sub()    -> sum()      | -> div()
+# DARKFLATS            \-> sub()  -> sum()                           |
+#                                                                    |-> register() -> stack()
+#
+# * only subtract masterBias from darks if dark optimisation is used
+
+info "\n---- Generating config"
+if ! config="$(./00-config.sh "$@")"; then
+    die "Failed to configure"
+fi
+echo "Config:"
+echo "$config"
+
+lightDir=$(<<<$config jq -r '.lightDir // empty')
+noBias=$(<<<$config jq -r '.noBias // empty')
+noFlat=$(<<<$config jq -r '.noFlat // empty')
+noDark=$(<<<$config jq -r '.noDark // empty')
+biasesDir=$(<<<$config jq -r '.biasDir // empty')
+flatsDir=$(<<<$config jq -r '.flatDir // empty')
+darksDir=$(<<<$config jq -r '.darkDir // empty')
+masterBias=$(<<<$config jq -r '.masterBias // empty')
+masterFlat=$(<<<$config jq -r '.masterFlat // empty')
+masterDark=$(<<<$config jq -r '.masterDark // empty')
+
+setConfig () {
+    cfg="$1"
+    name="$2"
+    value="$3"
+
+    <<<$cfg jq ".$name = \"$value\""
+}
 
 
-if [[ -z $noBiases && -z $masterBias ]]; then
-    true
+
+info "\n---- Generating masters"
+if [[ -n "$biasesDir" ]]; then
+    info "\n---- Generating master bias"
+    $dir/siril-processing/convertAndStackWithRejectionAndNoNorm.sh \
+        "$biasesDir" "bias_" "master-bias" || die "Failed to generate master bias";
+    masterBias="$biasesDir/master-bias.fit"
+
+    config=$(setConfig "$config" "masterBias" "$masterBias")
+    good "Created master bias $masterBias"
 fi
 
-if [[ -z $noFlats && -z $masterFlat ]]; then
+
+if [[ -n "$flatsDir" ]]; then
+    info "\n---- Generating master flat"
     if [[ -z $masterBias ]]; then
-        true
+        echo "WARNING generating master flat without using master bias"
+        $dir/siril-processing/convertAndStack.sh \
+            -d "$flatsDir" -s "flat_" -a "rej" -n "-norm=mul" -o "master-flat" || die "Failed to generate master flat";
+    else
+        bias="$(realpath --relative-to "$flatsDir" "$masterBias")"
+        echo "Using calibrating flats with bias $bias"
+        $dir/siril-processing/convertAndPreprocessWithCalibration.sh \
+            "$flatsDir" "flat_" "-bias=$bias" || die "Failed to preprocess flats";
+        $dir/siril-processing/stack.sh \
+            -d "$flatsDir" -s "pp_flat_" -n "-norm=mul" -o "master-flat" || die "Failed to stack flats";
     fi
+    masterFlat="$flatsDir/master-flat.fit"
+
+    config=$(setConfig "$config" "masterFlat" "$masterFlat")
+    good "Created master flat $masterFlat"
 fi
 
 
+if [[ -n "$darksDir" ]]; then
+    info "\n---- Generating master dark"
+    if [[ -n $masterBias ]]; then
+        echo "Not subtracting master bias from master dark"
+        echo "Light frames SHOULD NOT BE callibrated with master bias unless dark optimisation is used"
+    fi
+
+    $dir/siril-processing/convertAndStack.sh \
+        -d "$darksDir" -s "dark_" -a rej -n "-nonorm" -o "master-dark" || die "Failed to generate master dark";
+    masterDark="$darksDir/master-dark.fit"
+
+    config=$(setConfig "$config" "masterDark" "$masterDark")
+    good "Created master dark $masterFlat"
+fi
+echo "$config">.config.json
---
+++ siril-processing/stack.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -u
+
+good () { echo -e "\e[32m$*\e[0m"; }
+info () { echo -e "\e[34m$*\e[0m"; }
+errr () { echo -e "\e[31m$*\e[0m"; }
+
+die () { errr "${1:-""}" >&2; exit "${2:-1}"; }
+usage () { die "usage: $0 -d subs-dir -s sequence-name -o output-file [-n normalisation] [-a algorithm] [-r rejection]"; }
+
+dir=$(dirname "$0")
+
+
+subsDir=
+sequenceName=
+outputFile=
+algorithm="rej"
+normalisation="-nonorm"
+rejection="w 4 3"
+while getopts "d:s:o:n:a:r:" i; do
+    case "$i" in
+        d) subsDir="${OPTARG}" ;;
+        s) sequenceName="${OPTARG}" ;;
+        o) outputFile="${OPTARG}" ;;
+        n) normalisation="${OPTARG}" ;;
+        a) algorithm="${OPTARG}" ;;
+        r) rejection="${OPTARG}" ;;
+        -) break ;;
+        ?) usage ;;
+        *) usage ;;
+    esac
+done
+[[ -n $subsDir ]] || usage;
+[[ -n $sequenceName ]] || usage;
+[[ -n $outputFile ]] || usage;
+
+
+script="requires 0.99.9
+stack $sequenceName $algorithm $rejection $normalisation -out=$outputFile.fit"
+
+
+trap 'rm -f $subsDir/{${sequenceName}*,*.seq}' EXIT
+if ! "$dir/sirilWrapper.sh" "$subsDir" "$script"; then
+    die "Siril processing failed";
+fi
